27bc58061b6a68f0e3399c3548c370b91fc40db6
unknown <julia.martin97@gmx.de>
2022-05-20 12:45:21 +0200

    Startbildschirm Design und Buttonobjekte

--- app/src/main/java/com/example/multi_purpose_app/MainActivity.java
+++ app/src/main/java/com/example/multi_purpose_app/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.multi_purpose_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Buttons
+    LinearLayout btnGamble;
+    LinearLayout btnNotes;
+    LinearLayout btnRandom;
+    LinearLayout btnStopwatch;
+    LinearLayout btnTodo;
+    LinearLayout btnStudycards;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // OnClickListener für Buttons
+        btnGamble = (LinearLayout) findViewById(R.id.btnGamble);
+        btnGamble.setOnClickListener(this);
+
+        btnNotes = (LinearLayout) findViewById(R.id.btnNotes);
+        btnNotes.setOnClickListener(this);
+
+        btnRandom = (LinearLayout) findViewById(R.id.btnRandom);
+        btnRandom.setOnClickListener(this);
+
+        btnStopwatch = (LinearLayout) findViewById(R.id.btnStopwatch);
+        btnStopwatch.setOnClickListener(this);
+
+        btnTodo = (LinearLayout) findViewById(R.id.btnTodo);
+        btnTodo.setOnClickListener(this);
+
+        btnStudycards = (LinearLayout) findViewById(R.id.btnStudycard);
+        btnStudycards.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
