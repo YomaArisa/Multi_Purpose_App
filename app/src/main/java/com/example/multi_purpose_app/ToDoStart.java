e7dca03a3a98f7a08452b932f13eaac16c846cce
unknown <julia.martin97@gmx.de>
2022-06-11 17:55:35 +0200

    Layout Dummys und Java Klassen

---
+++ app/src/main/java/com/example/multi_purpose_app/ToDoStart.java
@@ -0,0 +1,14 @@
+package com.example.multi_purpose_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToDoStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_start);
+    }
+}
