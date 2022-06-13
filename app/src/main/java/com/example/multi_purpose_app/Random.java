2aa448e1827c4f1eb48b06888d665e5a99287752
Julia Martin <julia.martin@gmx.de>
2022-06-13 10:52:19 +0200

    Bugfixes

--- app/src/main/java/com/example/multi_purpose_app/Random.java
+++ app/src/main/java/com/example/multi_purpose_app/Random.java
@@ -2,7 +2,12 @@ package com.example.multi_purpose_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -36,20 +41,43 @@ public class Random extends AppCompatActivity implements View.OnClickListener {
         // Von Eingabe nach ID festlegen
         editStart = (EditText) findViewById(R.id.randomStartValue);
 
+        // Textfarbe nach Eingabe ändern
+        editStart.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    editStart.setTextColor(Color.WHITE);
+                }
+            }
+        });
+
         // Bis Eingabe nach ID festlegen
         editEnd = (EditText) findViewById(R.id.randomEndValue);
+
+        // Textfarbe nach Eingabe ändern
+        editEnd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    editEnd.setTextColor(Color.WHITE);
+                }
+            }
+        });
 
         // Ergebnis nach ID festlegen
         result = (TextView) findViewById(R.id.randomResultValue);
     }
 
+
+
     @Override
     public void onClick(View v) {
-        calcResult();
+                calcResult();
     }
 
     private void calcResult() {
         int start = Integer.parseInt(editStart.getText().toString());
+
         int end = Integer.parseInt(editEnd.getText().toString());
 
         // Zufallszahl von x bis y berechnen
